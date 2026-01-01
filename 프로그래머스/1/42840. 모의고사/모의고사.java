a00ff23a37a296d4f90ef81e4fd8c3e79718920a
yongsang930 <77559274+yongsang930@users.noreply.github.com>
2026-01-02 02:04:30 +0900

    [level 1] Title: 모의고사, Time: 1.71 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+      
+        // 1, 2, 3, 4, 5
+        // 2, 1, 2, 3, 2, 4, 2, 5
+        // 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
         
-        // 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5     
-        // 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5
-        // 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+        int[] arr1 = new int[]{1,2,3,4,5};
+        int[] arr2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] A = new int[]{1, 2, 3, 4, 5};
-        int[] B = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        int[] C = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int[] count = new int[3];
+        int[] count = new int[3];        
         
         for(int i = 0; i < answers.length; i++){
-            if(answers[i] == A[i%A.length]) count[0]++;
-            if(answers[i] == B[i%B.length]) count[1]++;
-            if(answers[i] == C[i%C.length]) count[2]++;
+            if(answers[i] == arr1[i%arr1.length]) count[0]++;
+            if(answers[i] == arr2[i%arr2.length]) count[1]++;
+            if(answers[i] == arr3[i%arr3.length]) count[2]++;
         }
-        
+              
         int max = Math.max(count[0], Math.max(count[1], count[2]));
-        List<Integer> list = new ArrayList();
+        
+        List<Integer> list = new ArrayList<Integer>();
         
         for(int i = 0; i < count.length; i++){
             if(max == count[i]){
@@ -28,14 +29,14 @@ class Solution {
             }
         }
         
-        int[] intArr = new int[list.size()];
+        int[] arr = new int[list.size()];
         
         for(int i = 0; i < list.size(); i++){
-            intArr[i] = list.get(i);
-        }  
+            arr[i] = list.get(i);
+        }
         
-        Arrays.sort(intArr);
+        Arrays.sort(arr);
         
-        return intArr;
+        return arr;
     }
 }
