5ab19bd5858218c4e32d709750d7d1c91797830f
yongsang930 <77559274+yongsang930@users.noreply.github.com>
2026-01-01 12:26:00 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -4,15 +4,16 @@ class Solution {
     public int solution(int[] number) {
         int count = 0;
 
-        for(int i = 0; i < number.length; i++){
+        for(int i = 0; i < number.length; i++){           
             for(int j = i+1; j < number.length; j++){
                 for(int k = j+1; k < number.length; k++){
-                    if((number[i] +  number[j] +  number[k]) == 0){
-                        count++;
-                    }
+                    int sum = number[i] + number[j] + number[k];
+                            
+                    if(sum == 0) count++;
                 }
             }
         }
+        
         return count;
     }
 }
