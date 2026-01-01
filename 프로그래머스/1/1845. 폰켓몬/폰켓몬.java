2875d39566a5658ff26e1dd61bc75ea7572b0fa5
yongsang930 <77559274+yongsang930@users.noreply.github.com>
2026-01-02 01:06:07 +0900

    [level 1] Title: 폰켓몬, Time: 6.38 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        //  총 N 마리의 폰켓몬 중에서 N/2마리
-        // 폰켓몬은 종류에 따라 번호를 붙여 구분
-        // 최대한 많은 종류의 포켓몬을 포함한 N/2마리
-        // N마리 포켓몬의 종류번호가 담
                 
-        Set set = new HashSet();
+        Set<Integer> set = new HashSet<Integer>();
         
         for(int i = 0; i < nums.length; i++){
             set.add(nums[i]);
