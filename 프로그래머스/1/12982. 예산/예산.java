ed9b73ef73f7352097449be2a7f22c42ead021c8
yongsang930 <77559274+yongsang930@users.noreply.github.com>
2026-01-01 23:23:31 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+       
+        Arrays.sort(d);
+        
+        int sum = 0;
+        int count = 0;
+        
+        for (int i = 0; i < d.length; i++) {
+            if (sum + d[i] > budget) {
+                break;
+            }
+            sum += d[i];
+            count++;
+        }
+        
+        return count;
+    }
+}
