df95e6aec670ad8e6f2e1a1b2133bdf14fb146a4
yongsang930 <77559274+yongsang930@users.noreply.github.com>
2026-01-03 01:59:36 +0900

    [level 2] Title: 올바른 괄호, Time: 17.72 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,14 +2,31 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stk = new Stack<>();
+//         boolean answer = true;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stk.push('(');
+//         int chk = 0;
+        
+//         for(int i = 0; i < s.length(); i++){
+//             if( '(' == s.charAt(i)) {
+//                 chk++;
+//             } else {
+//                 chk--;
+//             }
+//             if(chk < 0) return false;
+          
+//         }
+        
+        Stack<Character> stk = new Stack<Character>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if( '(' == s.charAt(i)){
+                stk.push(s.charAt(i));
             } else {
-                if (stk.isEmpty()) return false;
-                stk.pop();
+                if(!stk.isEmpty()){
+                    stk.pop();
+                } else {
+                    return false;
+                }
             }
         }
 
